305fbda18d778b3e1a6c6494a98236ec6b890e78
unknown <danlev2611@gmail.com>
2022-11-22 16:49:59 +0200

    suggestions admin access

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -9,6 +9,10 @@ const protect = asyncHandler(async(req,res,next)=>{
         try {
             token = req.headers.authorization.split(" ")[1]
             const decoded = jwt.verify(token,process.env.JWT_SECRET)
+            if(decoded.isAdmin){
+              req.user = decoded
+              next()
+            }
             req.user = await User.findById(decoded.id)
             next()
         } catch (error) {
