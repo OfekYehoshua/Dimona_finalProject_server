b7dad41179d5c52fa6788bf58896ef6b80e30b46
unknown <danlev2611@gmail.com>
2022-11-22 17:14:59 +0200

    admin access

--- routes/suggestionsRoute.js
+++ routes/suggestionsRoute.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const router = express.Router()
 const {allSuggestions,addSuggestion,deleteSuggestion} = require('../controllers/suggestinsController')
-const { protect, verifyTokenAndAuthorization } = require("../middleware/authMiddleware")
+const { protect, verifyTokenAndAuthorization,verifyTokenAdmin } = require("../middleware/authMiddleware")
 
 
 router.post('/:id',verifyTokenAndAuthorization,addSuggestion)
-router.get('/',  allSuggestions)
-router.delete('/', deleteSuggestion)
+router.get('/',verifyTokenAdmin,allSuggestions)
+router.delete('/',verifyTokenAdmin,deleteSuggestion)
 
 module.exports = router
